809f72e4c5c25eb8d9fadfc9d5ff1ea88dece6a3
Abdool-Zaid <aztoorabally7447@gmail.com>
2023-04-17 20:43:40 +0200

    cors

--- script.js
+++ script.js
@@ -2,15 +2,14 @@ let app = document.querySelector("#app")
 let i
 let data= localStorage.data?localStorage.data:[]
 let levelSelector=document.createElement("select")
-import jishoApi from './node_modules/unofficial-jisho-api';
-// let jisho 
-
-//     jisho= new jishoApi();
+// import jishoApi from './node_modules/unofficial-jisho-api';
+// let jisho = new jishoApi();
 
 
-// jisho.searchForPhrase('日').then(result => {
-//   console.log(result);
-// });
+
+// // jisho.searchForPhrase('日').then(result => {
+// //   console.log(result);
+// // });
 
 
 levelSelector.id="levelSelector"
@@ -29,5 +28,17 @@ app.append(levelSelector)
 levelSelector= document.querySelector("#levelSelector")
 levelSelector.addEventListener("change",()=>{
     let level = levelSelector.value
+    let url= 'https://jisho.org/api/v1/search/words?keyword=%23jlpt-'+ level
+    console.log(url)
+fetch(url,{
+    method: "GET", 
+    mode: "no-cors",
+    headers: {
+      "Content-Type": "application/json",
+    }
+  })
+.then(res=>{console.log(res)})
+
+
 
 })
